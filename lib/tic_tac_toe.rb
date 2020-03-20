30aabdaf6dac1b7f9d46f4921de9eb67da3a9c36
Kevin Minutti <minutti70@gmail.com>
2020-03-20 13:43:25 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,28 @@
+
+class TicTacToe
+  def initialize(board = nil)
+    @board = board || Array.new(9, " ")
+  end
+
+  WIN_COMBINATIONS = [
+[0,1,2], # Top row
+[3,4,5], # Middle row
+[6,7,8], # Bottom row
+[0,3,6], # Left row
+[1,4,7], # Center row
+[2,5,8], # Right row
+[2,4,6], # Foward diagonal
+[0,4,8]  # Backward diagonal
+]
+
+def display_board(board)
+  @board = board
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+
+end
