a95fff28a9692f96572887824aaa9961d5f9dede
Kevin Minutti <minutti70@gmail.com>
2020-03-20 13:55:26 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,7 +15,6 @@ class TicTacToe
 [0,4,8]  # Backward diagonal
 ]
 
-
 def display_board
   puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
   puts "-----------"
@@ -23,5 +22,12 @@ def display_board
   puts "-----------"
   puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
 end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+
+
 
 end
