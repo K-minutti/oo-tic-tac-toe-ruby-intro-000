c5a385773eb2b2d7f8ee7ffc34aae3d795c82ef2
Kevin Minutti <minutti70@gmail.com>
2020-03-20 14:07:21 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -130,9 +130,9 @@ def play
     turn
   end
 
-  if won?(board)
-    puts "Congratulations #{winner(board)}!"
-  elsif draw?(board)
+  if won?
+    puts "Congratulations #{winner}!"
+  elsif draw?
     puts "Cat's Game!"
   end
 end
